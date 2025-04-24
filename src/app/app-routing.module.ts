4b9d172bedec60b8a635c7f154a4781c0f377e7a
Bohdan Diakiv <bodichka322@gmail.com>
2025-04-24 11:35:41 +0300

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,15 @@
 import { Routes } from '@angular/router';
+import { CourseFormComponent, LoginFormComponent, RegistrationFormComponent } from './shared/components';
+import { CoursesListComponent } from './features/courses/courses-list/courses-list.component';
+import { CourseInfoComponent } from './features/course-info/course-info.component';
 
-export const routes: Routes = [
-    
-];
+const routes: Routes = [
+    { path: 'login', component: LoginFormComponent },
+    { path: 'registration', component: RegistrationFormComponent },
+    { path: 'courses', component: CoursesListComponent },
+    { path: 'courses/add', component: CourseFormComponent },
+    { path: 'courses/:id', component: CourseInfoComponent },
+    { path: 'courses/edit/:id', component: CourseFormComponent },
+    { path: '', pathMatch: 'full', redirectTo: 'courses' },
+    { path: '**', redirectTo: 'courses' },
+  ];
